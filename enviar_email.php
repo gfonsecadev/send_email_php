a6654233730b728aee1046a82e4be5df8bfab27e
Gilmar <90278833+gfonsecadev@users.noreply.github.com>
2024-02-27 15:54:19 -0300

    configuração variaveis de configuração

--- enviar_email.php
+++ enviar_email.php
@@ -51,8 +51,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'gildrive04@gmail.com';                     //SMTP username
-    $mail->Password   = 'mxxozgrzoqqcqjjd';                               //SMTP password
+    $mail->Username   = 'gilmar.testes05@gmail.com';                     //SMTP username
+    $mail->Password   = 'gil.teste05';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
